27f3460ace453ec53f009b27a0a65c2ab0e65ebe
Kevlo57 <kevlo57240@gmail.com>
2020-11-09 14:06:00 +0100

    Mise en place du personnage

--- FonctionSDL.cpp
+++
@@ -1,7 +0,0 @@
-#include "functions_SDL.h"
-
-SDL_Texture* load_image(const char* filename, SDL_Renderer* renderer){
-	SDL_Surface* surf = SDL_LoadBMP(filename);
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,surf);
-	return texture;
-}
